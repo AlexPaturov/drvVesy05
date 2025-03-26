608a9347006adcddaea4fd5da9df12144b976670
AlexPaturov <alexey.paturov@gmail.com>
2025-03-26 12:33:44 +0200

    Add project files.

---
+++ Connection.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace drvVesy05
+{
+    public class Connection
+    {
+        public Socket ARMsocket;
+        public Socket _moxaTC;
+        private bool _isfree = true;
+        private Dictionary<string, string> _d;
+        private bool _keepOpen = true;
+        private static readonly log4net.ILog logger = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public Connection()
+        {
+        }
+
+        public Connection(
+            Socket soc,
+            Dictionary<string, string> d,
+            Socket moxaTC)
+        {
+            StartConnection(soc, d, moxaTC);
+        }
+
+
+        public bool IsFree() => _isfree;
+
+
+        public void StopRequest()
+        {
+            _keepOpen = false;
+        }
+
+
+        public bool StartConnection(
+          Socket soc,
+          Dictionary<string, string> d,
+          Socket moxaTC)
+        {
+            ARMsocket = soc;
+            _moxaTC = moxaTC;
+            _d = d;
+            _isfree = false;
+
+            if (!_moxaTC.Connected)
+            {
+                logger.Info("Trying to connect to weighter ARM " + _d["moxaHost"] + ":" + _d["moxaPort"]);
+                try
+                {
+                    _moxaTC = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp); // так себе решение
+                    _moxaTC.ReceiveTimeout = 500;
+                    _moxaTC.SendTimeout = 500;
+                    _moxaTC.Connect(_d["moxaHost"], int.Parse(_d["moxaPort"]));
+                }
+                catch (Exception ex)
+                {
+                    logger.Error(ex);
+
+                    if (ARMsocket.Connected)
+                    {
+                        // протестировать throw exception 20.05.2024
+                        // ARMsocket.Shutdown(SocketShutdown.Both); // не тестировал
+                        ARMsocket.Close();
+                    }
+                    // throw ex;
+                }
+                logger.Info("Connection to controller - OK " + _moxaTC.RemoteEndPoint.ToString());
+            }
+            new Thread(new ThreadStart(Tranceiver)).Start();
+            return true;
+        }
+
+        private void Tranceiver()
+        {
+            byte[] buffer = new byte[8192];
+            _keepOpen = true;
+            string cliCmd = string.Empty;
+
+            logger.Info("Moxa connected from " + _d["moxaHost"] + ":" + int.Parse(_d["moxaPort"]));
+
+            while (_keepOpen)
+            {
+                bool transferOccured = false; // флаг свершения передачи данных клиент <-> контроллер
+
+                // ------------------------------ от клиента пришёл запрос begin -----------------------------------------------------------------   
+                int colByteARM = LimitTo(ARMsocket.Available, 8192);
+                if (colByteARM > 0) // 
+                {
+                    ARMsocket.Receive(buffer, colByteARM, SocketFlags.None);
+                    byte[] ARMbyteArr = new byte[colByteARM];                                               // установил размерность нового массива
+                    Buffer.BlockCopy(buffer, 0, ARMbyteArr, 0, colByteARM);
+                    logger.Info("ARM query string: " + Encoding.GetEncoding(1251).GetString(ARMbyteArr));   // строка запроса прикладного ПО драйверу
+
+                    byte[] controllerCommand = DecodeClientRequestToControllerCommand(buffer, colByteARM);  // Верну null если команда не корректная, иначе - команду для контроллера
+                    if (controllerCommand != null)                                                          // команда корректна -> отправляем устройству
+                    {
+                        try
+                        {
+                            _moxaTC.Send(controllerCommand, controllerCommand.Length, SocketFlags.None);    // запрос драйвера контроллеру
+                            logger.Info(Encoding.GetEncoding(1251).GetString(controllerCommand));           // команда контроллеру
+                            Thread.Sleep(600);                                                              // подождём пока данные прийдут. На 200 - сыплет ошибки.
+                            transferOccured = true;                                                                    // передача данных контроллеру была
+                        }
+                        catch (SocketException ex)
+                        {
+                            if (ex.SocketErrorCode == SocketError.TimedOut)
+                            {
+                                Exception exInner = new Exception("Прибор не ответил на запрос");           // Согласно спецификации - ловить таймаут
+                                byte[] errorByteArr = XMLFormatter.GetError(exInner, 1);                    // Отформатировал ошибку в XML формат. 
+                                ARMsocket.Send(errorByteArr, errorByteArr.Length, SocketFlags.None);        // Отправляем в АРМ весов XML в виде byte[].
+                            }
+                            logger.Error(ex);
+                        }
+
+                    }
+                    else                                                                        // формирование для клиента сообщения об ошибке в его запросе.
+                    {
+                        Exception ex = new Exception("Ошибка обработки запроса");               // Согласно спецификации.
+                        byte[] errorByteArr = XMLFormatter.GetError(ex, 2);                     // Отформатировал ошибку в XML формат. 
+                        ARMsocket.Send(errorByteArr, errorByteArr.Length, SocketFlags.None);    // Отправляем в АРМ весов XML в виде byte[].
+                        logger.Error(Encoding.GetEncoding(1251).GetString(errorByteArr));       // пишу ответ для арма весов.
+                    }
+                }
+                // ------------------------------ от клиента пришёл запрос end --------------------------------------------------------------
+
+                // ------------------------------ от моксы пришёл ответ начало --------------------------------------------------------------------- 
+                int colByteFromMoxa = LimitTo(_moxaTC.Available, 8192);
+                if (colByteFromMoxa > 0)
+                {
+                    try
+                    {
+                        _moxaTC.Receive(buffer, colByteFromMoxa, SocketFlags.None);
+                    }
+                    catch (SocketException ex)
+                    {
+                        logger.Error(ex);
+
+                        if (ex.SocketErrorCode == SocketError.TimedOut)                                 // по таймауту - делаю "своё" исключение
+                        {
+                            Exception exInner = new Exception("Прибор не ответил на запрос");           // Согласно спецификации.
+                            byte[] errorByteArr = XMLFormatter.GetError(exInner, 1);                    // Отформатировал ошибку в XML формат. 
+                            ARMsocket.Send(errorByteArr, errorByteArr.Length, SocketFlags.None);        // Отправляем в АРМ весов XML в виде byte[].
+                        }
+                    }
+
+                    #region only for show buffer data to textbox
+                    byte[] moxaArr = new byte[colByteFromMoxa];                                         // установил размерность нового массива
+                    Buffer.BlockCopy(buffer, 0, moxaArr, 0, colByteFromMoxa);                           // копирую данные в промежуточный массив для отображения
+                    logger.Info(Encoding.GetEncoding(1251).GetString(moxaArr)); // ответ драйвера прикладному ПО
+                    #endregion
+
+                    byte[] btArr = null;
+                    try
+                    {
+                        btArr = XMLFormatter.getStatic(moxaArr);                                        // Если возникла ошибка при разборе сообщения - кидаю исключение.
+                    }
+                    catch (Exception ex)
+                    {
+                        btArr = XMLFormatter.GetError(ex, 100);                                         // Отформатировал ошибку в XML формат, перевёл в byte[]
+                    }
+
+                    ARMsocket.Send(btArr, btArr.Length, SocketFlags.None);
+
+                    transferOccured = true;
+                }
+                // ------------------------------ от моксы пришёл ответ окончание ---------------------------------------------------------------------
+
+                if (ARMsocket.Poll(3000, SelectMode.SelectRead) & ARMsocket.Available == 0)
+                {
+                    logger.Info("Lost connection to weighter ARM " + ARMsocket.RemoteEndPoint.ToString());
+                    _keepOpen = false;
+                }
+
+                if (!transferOccured)
+                    Thread.Sleep(1);
+            }
+
+            if (_moxaTC.Connected)
+            {
+                logger.Info("Connect to controller is closed " + _moxaTC.RemoteEndPoint.ToString());
+                _moxaTC.Close();
+            }
+
+            logger.Info("Connect to weighter ARM is closed " + ARMsocket.RemoteEndPoint.ToString()); // отключение от ARM весов
+            ARMsocket.Close();
+            _isfree = true;
+        }
+
+        private int LimitTo(int i, int limit) => i > limit ? limit : i;
+
+        private byte[] DecodeClientRequestToControllerCommand(byte[] cliBuffer, int dataLength)
+        {
+            string controllerCommand = string.Empty;
+            byte[] clientComandArr = new byte[dataLength];                     // установил размерность массива для команды клиента
+            Buffer.BlockCopy(cliBuffer, 0, clientComandArr, 0, dataLength);    // копирую данные в промежуточный массив 
+
+            switch (Encoding.GetEncoding(1251).GetString(clientComandArr))
+            {
+                case "<Request method='set_mode' parameter='Static'/>":     // 1)
+                    controllerCommand = null;
+                    break;
+
+                case "<Request method='checksum'/>":                        // 2)
+                    controllerCommand = null;
+                    break;
+
+                case "<Request method='get_static'/>":                      // 3) получить вес, взвешивание в статике
+                    controllerCommand = "F#1" + "\r\n";
+                    break;
+
+                case "<Request method='set_zero' parameter='0'/>":          // 4)
+                    controllerCommand = null;
+                    break;
+
+                case "<Request method='restart_weight'/>":                  // 5)
+                    controllerCommand = null;
+                    break;
+
+                default:                                                    // 6) Команда полученная от клиента - не распознана.
+                    controllerCommand = null;
+                    break;
+            }
+
+            if (!string.IsNullOrEmpty(controllerCommand) && controllerCommand.Length > 0)
+            {
+                return Encoding.GetEncoding(1251).GetBytes(controllerCommand);
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+    }
+}
---
+++ XMLFormatter.cs
@@ -0,0 +1,274 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Xml;
+
+/*
+    Привожу полученные от контроллера данные к требуемому по спецификации формату, для ответа АРМ(у) весов.
+ */
+
+namespace drvVesy05
+{
+    public static class XMLFormatter
+    {
+
+        // Получить результат статического взвешивания.
+        public static byte[] getStatic(byte[] bInput) 
+        {
+            if (bInput != null) 
+            { 
+                Dictionary<string, string> preparedAnswer = RawToXML(System.Text.Encoding.Default.GetString(bInput) );
+
+                XmlDocument xmlDoc = new XmlDocument();                                                     
+                XmlDeclaration xmlDeclaration = xmlDoc.CreateXmlDeclaration("1.0", "UTF-8", null);
+            
+                XmlElement rootResponse = xmlDoc.CreateElement("Response");                                 
+                xmlDoc.AppendChild(rootResponse);                                                           
+
+                    XmlElement ch1_State = xmlDoc.CreateElement("State");                                   
+                    ch1_State.InnerText = "Success";
+                    rootResponse.AppendChild(ch1_State);
+
+                    XmlElement ch1_CheckSumZero = xmlDoc.CreateElement("CheckSumZero");                     
+                    ch1_CheckSumZero.InnerText = "0";
+                    rootResponse.AppendChild(ch1_CheckSumZero);
+
+                    XmlElement ch1_CheckSumWeight = xmlDoc.CreateElement("CheckSumWeight");
+                    ch1_CheckSumWeight.InnerText = "0";
+                    rootResponse.AppendChild(ch1_CheckSumWeight);
+
+                        XmlElement ch2_StaticData = xmlDoc.CreateElement("StaticData");
+                        rootResponse.AppendChild(ch2_StaticData);
+
+                        XmlElement ch3_Processed = xmlDoc.CreateElement("Processed");
+                        ch3_Processed.InnerText = "1";
+                        ch2_StaticData.AppendChild(ch3_Processed);
+
+                        XmlElement ch3_Npp = xmlDoc.CreateElement("Npp");
+                        ch3_Npp.InnerText = "1";
+                        ch2_StaticData.AppendChild(ch3_Npp);
+
+                        XmlElement ch3_Number = xmlDoc.CreateElement("Number");
+                        ch3_Number.InnerText = "0";
+                        ch2_StaticData.AppendChild(ch3_Number);
+
+                        XmlElement ch3_Date = xmlDoc.CreateElement("Date");
+                        ch3_Date.InnerText = preparedAnswer["Date"];
+                        ch2_StaticData.AppendChild(ch3_Date);
+
+                        XmlElement ch3_Time = xmlDoc.CreateElement("Time");
+                        ch3_Time.InnerText = preparedAnswer["Time"];
+                        ch2_StaticData.AppendChild(ch3_Time);
+
+                        XmlElement ch3_Brutto = xmlDoc.CreateElement("Brutto");
+                        ch3_Brutto.InnerText = preparedAnswer["Brutto"];
+                        ch2_StaticData.AppendChild(ch3_Brutto);
+
+                        XmlElement ch3_KolOs = xmlDoc.CreateElement("KolOs");
+                        ch3_KolOs.InnerText = "0";
+                        ch2_StaticData.AppendChild(ch3_KolOs);
+
+                        XmlElement ch3_Speed = xmlDoc.CreateElement("Speed");
+                        ch3_Speed.InnerText = "0";
+                        ch2_StaticData.AppendChild(ch3_Speed);
+
+                        XmlElement ch3_Platform1 = xmlDoc.CreateElement("Platform1");
+                        ch3_Platform1.InnerText = preparedAnswer["Platform1"];
+                        ch2_StaticData.AppendChild(ch3_Platform1);
+
+                        XmlElement ch3_Rail1_1 = xmlDoc.CreateElement("Rail1_1");
+                        ch3_Rail1_1.InnerText = "0";
+                        ch2_StaticData.AppendChild(ch3_Rail1_1);
+
+                        XmlElement ch3_Rail1_2 = xmlDoc.CreateElement("Rail1_2");
+                        ch3_Rail1_2.InnerText = "0";
+                        ch2_StaticData.AppendChild(ch3_Rail1_2); ;
+
+                        XmlElement ch3_Rail1_3 = xmlDoc.CreateElement("Rail1_3");
+                        ch3_Rail1_3.InnerText = "0";
+                        ch2_StaticData.AppendChild(ch3_Rail1_3);
+
+                        XmlElement ch3_Rail1_4 = xmlDoc.CreateElement("Rail1_4");
+                        ch3_Rail1_4.InnerText = "0";
+                        ch2_StaticData.AppendChild(ch3_Rail1_4);
+
+                        XmlElement ch3_Platform2 = xmlDoc.CreateElement("Platform2");
+                        ch3_Platform2.InnerText = preparedAnswer["Platform2"];
+                        ch2_StaticData.AppendChild(ch3_Platform2);
+
+                        XmlElement ch3_Rail2_1 = xmlDoc.CreateElement("Rail2_1");
+                        ch3_Rail2_1.InnerText = "0";
+                        ch2_StaticData.AppendChild(ch3_Rail2_1);
+
+                        XmlElement ch3_Rail2_2 = xmlDoc.CreateElement("Rail2_2");
+                        ch3_Rail2_2.InnerText = "0";
+                        ch2_StaticData.AppendChild(ch3_Rail2_2); ;
+
+                        XmlElement ch3_Rail2_3 = xmlDoc.CreateElement("Rail2_3");
+                        ch3_Rail2_3.InnerText = "0";
+                        ch2_StaticData.AppendChild(ch3_Rail2_3);
+
+                        XmlElement ch3_Rail2_4 = xmlDoc.CreateElement("Rail2_4");
+                        ch3_Rail2_4.InnerText = "0";
+                        ch2_StaticData.AppendChild(ch3_Rail2_4);
+
+                        XmlElement ch3_ShiftPop = xmlDoc.CreateElement("ShiftPop");
+                        ch3_ShiftPop.InnerText = preparedAnswer["ShiftPop"];
+                        ch2_StaticData.AppendChild(ch3_ShiftPop);
+
+                        XmlElement ch3_ShiftPro = xmlDoc.CreateElement("ShiftPro");
+                        ch3_ShiftPro.InnerText = preparedAnswer["ShiftPro"];
+                        ch2_StaticData.AppendChild(ch3_ShiftPro);
+
+                        XmlElement ch3_pravBort1_2 = xmlDoc.CreateElement("PravBort1_2");
+                        ch3_pravBort1_2.InnerText = preparedAnswer["PravBort1_2"];
+                        ch2_StaticData.AppendChild(ch3_pravBort1_2);
+
+                        XmlElement ch3_levBort3_4 = xmlDoc.CreateElement("LevBort3_4");
+                        ch3_levBort3_4.InnerText = preparedAnswer["LevBort3_4"];
+                        ch2_StaticData.AppendChild(ch3_levBort3_4);
+
+                        XmlElement ch3_Delta = xmlDoc.CreateElement("Delta");
+                        ch3_Delta.InnerText = preparedAnswer["Delta"];
+                        ch2_StaticData.AppendChild(ch3_Delta);
+
+                        XmlElement ch3_Type = xmlDoc.CreateElement("Type");
+                        ch3_Type.InnerText = "V";
+                        ch2_StaticData.AppendChild(ch3_Type);
+
+                return Encoding.GetEncoding(1251).GetBytes(xmlDoc.OuterXml);
+            }
+            else 
+            {
+                throw new Exception("Answer from device is incorrect. getStatic input == null");
+            }
+        }
+
+        // Получаю стандартный Exception и код по спецификации, возвращаю ошибку в установленном спецификацией формате XML.
+        public static byte[] GetError(Exception ex, int code) 
+        {
+            XmlDocument xmlDoc = new XmlDocument();                                                         
+            XmlDeclaration xmlDeclaration = xmlDoc.CreateXmlDeclaration("1.0", "UTF-8", null);
+
+            XmlElement rootResponse = xmlDoc.CreateElement("Response");                                     
+            xmlDoc.AppendChild(rootResponse);
+
+            XmlElement ch1_State = xmlDoc.CreateElement("State");                                           
+            ch1_State.InnerText = "Error";
+            rootResponse.AppendChild(ch1_State);
+
+                XmlElement ch2_ErrorDescription = xmlDoc.CreateElement("ErrorDescription");                 
+                rootResponse.AppendChild(ch2_ErrorDescription);
+
+                XmlElement ch3_ErrorCode = xmlDoc.CreateElement("ErrorCode");                               
+                ch3_ErrorCode.InnerText = code.ToString();
+                ch2_ErrorDescription.AppendChild(ch3_ErrorCode);
+
+                XmlElement ch3_ErrorText = xmlDoc.CreateElement("ErrorText");
+                ch3_ErrorText.InnerText = ex.Message;
+                ch2_ErrorDescription.AppendChild(ch3_ErrorText);
+
+            return Encoding.GetEncoding(1251).GetBytes(xmlDoc.OuterXml);
+        }
+
+        private static Dictionary<string, string> RawToXML(string input)
+        {
+            Dictionary<string, string> XMLtmp = new Dictionary<string, string>();
+
+            if (input.Contains("F#1"))                                                                                                  // 1
+            {
+                input = input.Substring(input.IndexOf("F#1") + 3, (input.Length - (input.IndexOf("F#1") + 3))).Trim();
+            }
+            else
+            {
+                throw new Exception($"Format of answer from device is incorrect. {input}");
+            }
+
+            string checkedInput = FixBSafterDate(input);
+            if (checkedInput == "0")
+            {
+                throw new ArgumentException($"Controller data is corrupted: {input}");
+            }
+
+            // Извлекаем дату
+            int firstSpace = checkedInput.IndexOf(" ");
+            if (firstSpace == -1) throw new Exception($"Invalid format: Missing date. {checkedInput}");
+
+            string datePart = checkedInput.Substring(0, firstSpace);
+            checkedInput = checkedInput.Substring(firstSpace + 1);
+
+            if (!DateTime.TryParseExact(datePart, "dd-MM-yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime parsedDate))
+            {
+                throw new Exception($"Invalid date format. {checkedInput}");
+            }
+
+            XMLtmp.Add("Date", parsedDate.ToString("dd/MM/yyyy", CultureInfo.InvariantCulture));
+
+            // Извлекаем время
+            int secondSpace = checkedInput.IndexOf(" ");
+            if (secondSpace == -1) throw new Exception($"Invalid format: Missing time. {checkedInput}");
+
+            string timePart = checkedInput.Substring(0, secondSpace);
+            checkedInput = checkedInput.Substring(secondSpace + 1);
+
+            XMLtmp.Add("Time", timePart);
+            XMLtmp.Add("Brutto", TonnsToKilos(ExtractWeight(checkedInput)));                                    // Извлекаем вес
+            XMLtmp.Add("Platform1", "0");
+            XMLtmp.Add("Platform2", "0");
+            XMLtmp.Add("PravBort1_2", "0");
+            XMLtmp.Add("LevBort3_4", "0");
+            XMLtmp.Add("ShiftPop", "0");
+            XMLtmp.Add("ShiftPro", "0");
+            XMLtmp.Add("Delta", "0");
+            return XMLtmp;
+        }
+
+        // Перевожу тонны в киллограммы, возвращаю в строковом представлении.
+        private static string TonnsToKilos(string inputTonns) 
+        {
+            if (!string.IsNullOrEmpty(inputTonns)) 
+            {
+                double outputKilos = 0;
+                if (double.TryParse(inputTonns, out outputKilos))
+                {
+                    return (outputKilos * 1000).ToString();
+                }
+                throw new Exception("Calculation mass is incorrect. | " + inputTonns +" |");
+            }
+            else
+            {
+                throw new Exception("Mass value is incorrect.");
+            }
+            
+        }
+
+        // Для получения значения веса из строки от контроллера 5-х весов
+        static string ExtractWeight(string input)
+        {
+            string inpFixed = FixBSafterDate(input);
+            Regex numberRegex = new Regex(@"\b\d{1,2}\.\d{3}\b|\b0\.\d{3}\b"); // Регулярное выражение для поиска валидных чисел после даты и времени
+            MatchCollection matches = numberRegex.Matches(inpFixed);
+
+            foreach (Match match in matches)
+            {
+                if (decimal.TryParse(match.Value, NumberStyles.AllowDecimalPoint, CultureInfo.InvariantCulture, out decimal value) && value >= 0.000m && value <= 59.999m)
+                {
+                    return match.Value;
+                }
+            }
+
+            return "0";
+        }
+
+        #region FixBSafterDate() Контроллер иногда теряет пробел после даты и времени - добавляем пробел
+        static string FixBSafterDate(string input)
+        {
+            if (input.Length < 18) return "0";
+            if (input[16] != ' ') return input.Insert(16, " ");
+            return input;
+        }
+        #endregion
+    }
+}
